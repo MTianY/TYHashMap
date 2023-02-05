70dcbbc34d3c1d68f64073a023dbf877080616b6
mty <mtystar@126.com>
2023-02-05 15:21:56 +0800

    扩容

--- Map/src/TYHashMap.java
+++ Map/src/TYHashMap.java
@@ -23,6 +23,9 @@ public class TYHashMap<K, V> implements TYMap<K, V> {
     // 为了提高效率, 使用 & 位运算取代 % 运算, 前提是将数组长度设计为 2 的幂次方
     private static final int DEFAULT_CAPACITY = 1 << 4;
 
+    // 装填因子. 超过 0.75 时, 哈希表桶数组扩容为原来的 2 倍
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+
     public TYHashMap() {
         // 默认给数组各最大容量
         table = new Node[DEFAULT_CAPACITY];
@@ -51,6 +54,9 @@ public class TYHashMap<K, V> implements TYMap<K, V> {
 
     @Override
     public V put(K key, V value) {
+
+        // 扩容
+        resize();
 
         // 取出元素索引
         int index = index(key);
@@ -200,6 +206,100 @@ public class TYHashMap<K, V> implements TYMap<K, V> {
                 rotateLeft(grand);
             }
         }
+    }
+
+    private void resize() {
+
+        // 装填因子: 节点总数量/桶数组长度
+        if (size / table.length <= DEFAULT_LOAD_FACTOR) return;
+
+        // 装填因子大于 0.75 时才扩容
+
+        Node<K,V> []oldTable = table;
+        table = new Node[oldTable.length << 1];
+
+        Queue<Node<K,V>> queue = new LinkedList<>();
+        for (int i = 0; i < oldTable.length; i++) {
+            if (oldTable[i] == null) continue;
+
+            queue.offer(oldTable[i]);
+            while (!queue.isEmpty()) {
+                Node<K,V> node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+
+                // 移动节点
+                moveNode(node);
+
+            }
+
+        }
+
+    }
+
+    private void moveNode(Node<K,V> newNode) {
+        // 重置节点
+        newNode.parent = null;
+        newNode.left = null;
+        newNode.right = null;
+
+        // 新节点颜色, 默认改为 RED
+        newNode.color = RED;
+
+        int index = index(newNode);
+        Node<K,V> root = table[index];
+        if (root == null) {
+            root = newNode;
+            table[index] = root;
+            afterPut(root);
+            return;
+        }
+
+        // 添加到新节点的红黑树上
+        Node<K,V> parent = root;
+        Node<K,V> node = root;
+        int cmp = 0;
+        K k1 = newNode.key;
+        int h1 = newNode.hashCode;
+        Node<K,V> result = null;
+        do {
+            parent = node;
+            K k2 = node.key;
+            int h2 = node.hashCode;
+            if (h1 > h2) {
+
+            } else if (h1 < h2) {
+
+            } else if (k1 != null && k2 != null
+                    && k1 instanceof Comparable
+                    && k1.getClass() == k2.getClass()
+                    && (cmp = ((Comparable) k1).compareTo(k2)) != 0
+                ) {
+
+            } else {
+                cmp = System.identityHashCode(k1) - System.identityHashCode(k2);
+            }
+
+            if (cmp > 0) {
+                node = node.right;
+            } else if (cmp < 0){
+                node = node.left;
+            }
+
+        } while (node != null);
+
+        if (cmp > 0) {
+            parent.right = newNode;
+        } else {
+            parent.left = newNode;
+        }
+        newNode.parent = parent;
+
+        afterPut(newNode);
     }
 
     @Override
